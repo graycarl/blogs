955459c18d52e3974c3b4d60b8e530b2b6260f10
Hongbo He <hongbo@Mays-MacBook-Air.local>
2018-02-27 23:15:50 +0800

    New version pelican

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,44 +2,37 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'hongbo'
-SITENAME = u'Hongbo He'
-SITESUBTITLE = u'月亮哪去了'
+AUTHOR = 'Hongbo He'
+SITENAME = 'I am HHB'
 SITEURL = ''
-# DISQUS_SITENAME = "blog.graycarl.me"
-DUOSHUO_SITENAME = "carlblog"
+SITESUBTITLE = u'月亮哪去了'
 
-TIMEZONE = 'Europe/Paris'
-DEFAULT_LANG = u'en'
+PATH = 'content'
 
-DISPLAY_CATEGORIES_ON_MENU = True
-DISPLAY_PAGES_ON_MEUN = False
+TIMEZONE = 'Asia/Shanghai'
 
-# Baidu Tongji
-BAIDU_TONGJI = True
+DEFAULT_LANG = 'zh'
 
 # Feed generation is usually not desired when developing
-# FEED_ALL_RSS = "feeds/all.rss.xml"
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python China', 'http://python-china.org/'))
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('GitHub', 'http://github.com/graycarl'),
-          ('Weibo', 'http://weibo.com/graycarl'),)
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 10
-SUMMARY_MAX_LENGTH = 20
+DEFAULT_PAGINATION = False
 
-FILES_TO_COPY = (('extra/CNAME', 'CNAME'),
-                 ('resources/hhbavatar.jpg', 'hhbavatar.jpg'))
-
-THEME = "pelican-hhb"
-# THEME = "/Users/carl/source/pelican-hhb/"
+THEME = 'pelican-hhb'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
